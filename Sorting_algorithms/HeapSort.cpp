60fefbfc625682dd9b2b78289e6f807e9466c3e3
Julia Sujka <272869@student.pwr.edu.pl>
2024-04-17 22:46:39 +0200

    dodanie komentarzy

--- Sorting_algorithms/HeapSort.cpp
+++ Sorting_algorithms/HeapSort.cpp
@@ -8,25 +8,25 @@ void HeapSort::heapify(int *arr, int n, int i) { //kopiowanie, n=ilosc liczb, i=
     int r = 2 * i + 2; //wzór na indeks prawego potomka
 
     if (l < n && arr[l] > arr[largest]) //sprawdzamy czy l nie jest końcem tablicy, i czy element o indeksie l nie jest większy od rodzica
-        largest = l; //jeśli jest to w miejsce
+        largest = l; // Aktualizacja largest na indeks lewego dziecka
 
     if (r < n && arr[r] > arr[largest]) //analogicznie
-        largest = r;
+        largest = r; // Aktualizacja largest na indeks prawego dziecka
 
-    if (largest != i) { //jesli indeks rodzica
-        std::swap(arr[i], arr[largest]);
-        heapify(arr, n, largest);
+    if (largest != i) { // Jeśli largest nie jest taki sam jak bieżący indeks i
+        std::swap(arr[i], arr[largest]); // Zamiana elementów na indeksach i i largest
+        heapify(arr, n, largest); // Rekurencyjne układanie kopca dla poddrzewa o korzeniu w largest
     }
 }
 
 long long HeapSort::heap_sort(int *arr, int n) {
     auto start = std::chrono::high_resolution_clock::now();
     for (int i = n / 2 - 1; i >= 0; i--) //tworzy kopiec
-        heapify(arr, n, i);
+        heapify(arr, n, i); // Budowanie kopca maksymalnego
 
     for (int i = n - 1; i > 0; i--) { // sortuje tablicę poprzez wymianę korzenia z ostatnim elementem i naprawianie kopca w części tablicy przedostatniej.
-        std::swap(arr[0], arr[i]);
-        heapify(arr, i, 0);
+        std::swap(arr[0], arr[i]); // Zamiana korzenia (największego elementu) z ostatnim elementem
+        heapify(arr, i, 0); // Układanie kopca dla zmniejszonego kopca
     }
     auto end = std::chrono::high_resolution_clock::now();
     return std::chrono::duration_cast<std::chrono::microseconds>(end - start).count();
